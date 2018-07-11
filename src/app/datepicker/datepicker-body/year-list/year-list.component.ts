0a802fab77970e62c9ef802f1703e3cd949155d0
Cadoz Olivier <link@macbook-pro-de-cadoz.home>
2018-07-11 22:50:18 +0200

    datepicker: permet de changer de list d'année

--- src/app/datepicker/datepicker-body/year-list/year-list.component.ts
+++ src/app/datepicker/datepicker-body/year-list/year-list.component.ts
@@ -12,26 +12,51 @@ export class YearListComponent implements OnInit {
   public yearActive: number;
 
   private date: Date;
+  private firstInit = true;
+  private fromStart = false;
+  private fromEnd = false;
 
   constructor(
     private readonly datepickerService: DatepickerService,
     private readonly cdr: ChangeDetectorRef
-  ) {
-    this.years = [];
-  }
+  ) {}
 
   ngOnInit() {
     this.datepickerService.date.subscribe(date => {
       this.date = date;
+      if (!this.firstInit) this.initFromBooleans(date);
       this.yearActive = date.getFullYear();
-      this.initYears(this.yearActive)
+      if (this.firstInit) this.firstInitYears(this.yearActive);
+      else if (this.fromStart && !this.years.includes(this.yearActive)) this.initYearsFromStart(this.yearActive);
+      else if (this.fromEnd && !this.years.includes(this.yearActive)) this.initYearsFromEnd(this.yearActive);
     });
   }
 
-  initYears(yearActive) {
-    for (let i = 7; yearActive - i <= yearActive + 8; i--) {
-      this.years.push(yearActive - i);
+  initYearsFromStart(year) {
+    console.log("From start");
+    this.years = [];
+    for (let i = 0; year + i < year + 16; i++) {
+      this.years.push(year + i);
     }
+    this.cdr.markForCheck();
+  }
+
+  initYearsFromEnd(year) {
+    console.log("From end");
+    this.years = [];
+    for (let i = 0; year - i > year - 16; i++) {
+      this.years.unshift(year - i);
+    }
+    this.cdr.markForCheck();
+  }
+
+  firstInitYears(year) {
+    console.log("First");
+    this.years = [];
+    for (let i = 7; year - i <= year + 8; i--) {
+      this.years.push(year - i);
+    }
+    this.firstInit = false;
     this.cdr.markForCheck();
   }
 
@@ -39,5 +64,17 @@ export class YearListComponent implements OnInit {
     this.date.setFullYear(newYear);
     this.datepickerService.date.next(this.date);
     this.datepickerService.headerAction.next("month");
+  }
+
+  initFromBooleans(date) {
+    const compareYear = date.getFullYear() > this.yearActive;
+    if (!this.fromStart && compareYear) {
+      this.fromStart = true;
+      this.fromEnd = false;
+    }
+    else if (!this.fromEnd && !compareYear) {
+      this.fromStart = false;
+      this.fromEnd = true;
+    }
   }
 }
